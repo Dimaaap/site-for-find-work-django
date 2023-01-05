f8ce3aea674cb2ac3b3f00f0d9d163f485b2e735
Dimaaap <procdima49@gmail.com>
2023-01-05 17:11:09 +0300

    Оформив дизайн головної сторінки,зробив міграції,створив суперкористувача.Почав роботу над системою реєстрації користувачів

--- worksite/worksite/settings.py
+++ worksite/worksite/settings.py
@@ -76,9 +76,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'uk-ukr'
 
 TIME_ZONE = 'UTC'
@@ -87,12 +84,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
